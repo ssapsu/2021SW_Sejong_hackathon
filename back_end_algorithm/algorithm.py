859ddc7e623e057c77a1dcc0a464a5e16c6f1e09
kyounghwan kim <79563142+KyongHwan-Kim@users.noreply.github.com>
2021-09-29 18:46:30 +0900

    Update algorithm.py

---
+++ back_end_algorithm/algorithm.py
@@ -0,0 +1,13 @@
+import queue
+
+graph = {}
+infinity = float("inf")
+cost = {}
+parents = {}
+processed = []
+
+def init()
+    global graph, infinity, costs, parents, processed
+
+    graph = {}
+    graph["DaeyangAI"]
